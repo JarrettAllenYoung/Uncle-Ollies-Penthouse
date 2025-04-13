10a4c5368f98f89447f9d8cc85d3711225573d2b
Jarrett Allen Young <young.jarrett@gmail.com>
2025-04-13 15:01:55 -0700

    Update picklerick.js

--- games/picklerick.js
+++ games/picklerick.js
@@ -37,7 +37,7 @@ function displayVictoryMess(moves) {
   // Check if the current difficulty is extreme (value "38")
   if (difficulty === "38") {
     movesMessage = "Amazing! You've conquered Extreme Difficulty in " + moves + " moves!<br>" +
-                   "<a href='https://yourprizelink.com'>Click here</a> to claim your prize";
+                   "<a href='https://uncleolliespenthouse.netlify.app/games/shuffle.html'>Click here</a> to claim your prize";
   } else {
     movesMessage = "You Moved " + moves + " Steps.";
   }
